35d24cda630e526d43f82a6350076c6101ea896e
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-15 11:08:17 -0300

    piano is back

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('*.js', function(req, res){
+	res.sendfile("public/js/"+req.path);
+});
+
+app.get('*.html', function(req,res){
+	res.sendfile("public/views/"+req.path);
+});
+
+app.get('*.css', function(req, res){
+	res.sendfile('public/css/'+req.path);
+});
+
+
+io.on('connection', function(socket){
+
+	socket.on("play", function(note){
+		console.log(note);
+		socket.broadcast.emit("play", note);
+	});
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
